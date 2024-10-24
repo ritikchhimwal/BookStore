e379f29763d2ef53e07c4847f375928933c241ec
Ritik Chhimwal <132690630+ritikchhimwal@users.noreply.github.com>
2024-10-24 13:50:04 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(cors(
     {
-        origin: ["https://book-store-frontend-khaki.vercel.app/"],
+        origin: ["https://book-store-frontend-khaki.vercel.app"],
         methods: ["POST","GET"],
         credentials: true
     }
