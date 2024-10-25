62727e05ccaea1f0dace80abe2182bb2197d715a
Ritik Chhimwal <132690630+ritikchhimwal@users.noreply.github.com>
2024-10-25 11:55:13 +0530

    Update Course.jsx

--- frontend/src/components/Course.jsx
+++ frontend/src/components/Course.jsx
@@ -10,7 +10,7 @@ const Course = () => {
     useEffect(()=>{
         const getBook = async()=>{
             try{
-                const res = await axios.get("book-store-one-delta.vercel.app/book");
+                const res = await axios.get("https://bookstore-backend-wch0.onrender.com/book");
                 console.log(res.data);
                 setBook(res.data);
             }
